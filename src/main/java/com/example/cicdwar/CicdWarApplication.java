f69a45e3c0516634d2a3058566529713fff9cd82
jihunchoe <chlwlgns524@naver.com>
2023-03-01 13:53:13 +0900

    first commit

---
+++ src/main/java/com/example/cicdwar/CicdWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdWarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdwar/CicdWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
